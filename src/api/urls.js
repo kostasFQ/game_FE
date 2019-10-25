0a2555a527b8ee8e3488f2fec8c84d4722fe1a41
Kanstantsin Asipenka <kanstantsin.asipenka@1pt.com>
2019-10-25 11:47:06 +0300

    return dev mode

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,4 +1,8 @@
-const url = () => process.env.REACT_APP_API_URL;
+const url = () => {
+  return process.env.REACT_APP_DEV ?
+    process.env.REACT_APP_API_URL_DEV :
+    process.env.REACT_APP_API_URL_PROD;
+}
 
 export const saveResultUrl = () => `${url()}/save`;
 export const leaderBoard = (count = 5) => `${url()}/leader-board?count=${count}`;
